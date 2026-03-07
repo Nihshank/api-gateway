9fb15fa0b84482a707b29476ae0a6ac545f0ffa2
Nihshank <nihshankluray@gmail.com>
2026-03-07 17:57:23 -0500

    feat: add dynamic handling for http methods

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,30 @@ const router = express.Router()
 const axios = require('axios')
 const registry = require('./registry.json')
 
-router.all('/:path', async (req, res) => {
+router.all('/:path{/*splat}', async (req, res) => {
     const path = req.params.path
+    const rest = req.params.splat ? `/${req.params.splat}` : ''
     const service = registry.services[path]
 
     if (!service){
         return res.status(404).json({ error: `Service '${path}' not found` })
     }
 
-    console.log(`Route to ${path} api`)
-    const response = await axios.get(service.url)
+    if (!service.methods.includes(req.method)){
+        return res.status(405).json({ error: `Method ${req.method} not allowed` })
+    }
+
+    const url = `${service.url}${rest}`
+
+    const response = await axios({
+        method: req.method,
+        url: url,
+        data: req.body,
+        headers: {
+            'Content-Type': req.headers['content-type'],
+            'Authorization': req.headers['authorization']
+        },
+    })
     res.send(response.data)
 })
 

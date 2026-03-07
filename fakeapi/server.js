4f1671ce5238e9bcc03379a4aa9afbe060ca2cec
Nihshank <nihshankluray@gmail.com>
2026-03-07 14:40:29 -0500

    feat: forward HTTP requests from Gateway to fakeAPI

--- fakeapi/server.js
+++ fakeapi/server.js
@@ -6,6 +6,9 @@ app.use(express.json())
 app.get('/fakeapi', (req, res) => {
     res.send("At fake api server\n")
 })
+app.get('/bogusapi', (req, res) => {
+    res.send("At bogus api server\n")
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on http://localhost:${PORT}`)
